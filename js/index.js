787f6af7dcf4ac44b6045c21eab30610cd5ed36e
yoge-esh <yogesh124khanal@gmail.com>
2021-12-29 22:12:19 +0545

    first commit

---
+++ js/index.js
@@ -0,0 +1,27 @@
+"use strict"; // protect us from making mistakes 
+
+
+// to print the code in console.log
+console.log('hello world');
+// console.log("hello world4");
+// console.log(`hello world5`);
+
+
+// var is use to declare a variable in js 
+var firstName = 'javaScript';
+// var is a keyword 
+// variables are case sensative 
+// use of variable 
+console.log(firstName);
+
+// change value 
+// while change the value i dont need to use var key word 
+firstName = 'javaScript for React js';
+console.log(firstName);
+
+
+// let varibles 
+let lastName = 'lastname';
+// let lastName = 'lastname'; // error because we have already use this name 
+lastName = 'khanal'
+console.log(lastName)
