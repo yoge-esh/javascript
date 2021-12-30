9f433f202faeb7ae15e5900f643b556d8d048c69
yoge-esh <yogesh124khanal@gmail.com>
2021-12-30 12:43:05 +0545

    indexing in js

--- js/index.js
+++ js/index.js
@@ -25,3 +25,25 @@ let lastName = 'lastname';
 // let lastName = 'lastname'; // error because we have already use this name 
 lastName = 'khanal'
 console.log(lastName)
+
+// constant :- its value cant be change  
+const pi = 3.1415
+console.log(pi);
+
+
+// string length 
+// string start from index 0
+let name = 'yogesh'
+console.log(name[2]);
+console.log(name[3]);
+console.log(name[1]);
+
+// length of string 
+let Name = 'This is a javascript course'
+console.log(Name.length); // count the lenth of string 
+
+// negative indexing
+console.log(Name[Name.length-1]);
+console.log(Name[Name.length-2]);
+console.log(Name[Name.length-3]);
+console.log(Name[Name.length-5]);
