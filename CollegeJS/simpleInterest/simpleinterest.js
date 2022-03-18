51705bccdd52f75f8823b6128f3257bfa214c743
yoge-esh <yogesh124khanal@gmail.com>
2022-03-18 23:29:59 +0545

    simple interest

---
+++ CollegeJS/simpleInterest/simpleinterest.js
@@ -0,0 +1,9 @@
+// simple interest calculator
+var principle = prompt("Enter principle amount: ");
+var rate = prompt("Enter rate of interest: ");
+var time = prompt("Enter time in years: ");
+var si = (principle * rate * time) / 100;
+alert("Simple interest is: " + si);
+console.log("\n")
+console.log("Simple interest is: " + si);
+document.write("<br>Simple interest is: " + si);
