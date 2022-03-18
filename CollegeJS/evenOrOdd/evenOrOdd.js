4196ccbdfdf45e95ff0ef0671664cd0c02ef36f9
yoge-esh <yogesh124khanal@gmail.com>
2022-03-18 23:25:10 +0545

    even or odd number

---
+++ CollegeJS/evenOrOdd/evenOrOdd.js
@@ -0,0 +1,12 @@
+// even or odd number
+var a =  prompt("Enter a number: ");
+if (a % 2 == 0) {
+    alert("Even number");
+    console.log(`you have enter a ${a} which is Even number`);
+    document.write(`you have enter a ${a} which is Even number`);
+}
+else {
+    alert("Odd number");
+    console.log(`you have enter a ${a} which is  Odd number`);
+    document.write(`you have enter a ${a} which is Odd number`);
+}
