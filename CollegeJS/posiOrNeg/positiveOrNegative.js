4e656254727ddca92d291c7bc21bec479f586941
yoge-esh <yogesh124khanal@gmail.com>
2022-03-18 23:28:20 +0545

    positive or Negative given number in html

---
+++ CollegeJS/posiOrNeg/positiveOrNegative.js
@@ -0,0 +1,17 @@
+// positive ot Negative numver 
+var a =  prompt("Enter a number: ");
+if (a > 0) {
+    alert("Positive number");
+    console.log(`you have enter a ${a} Positive number`);
+    document.write(`you have enter a ${a} Positive number`);
+}
+else if (a < 0) {
+    alert("Negative number");
+    console.log(`you have enter a ${a} Negative number`);
+    document.write(`you have enter a ${a} Negative number`);
+}
+else {
+    alert("Zero");
+    console.log(`you have enter a ${a} Zero`);
+    document.write(`you have enter a ${a} Zero`);
+}
