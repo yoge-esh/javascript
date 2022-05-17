65407410b678de830a97269844bf842499e0e698
yoge-esh <yogesh124khanal@gmail.com>
2022-05-17 10:56:29 +0545

    reverse string

---
+++ CollegeJS/reverse_string/reverse_string.js
@@ -0,0 +1,6 @@
+// write a program to to reverse the string 
+
+var value = prompt("Enter your name: ");
+var reverse = value.split("").reverse().join("");
+console.log(reverse);
+document.write(reverse);
